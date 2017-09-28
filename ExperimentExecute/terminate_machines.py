438f22a32a100790cf063d2806332510e392547a
liorbiu <liork.cryptobiu@gmail.com>
2017-09-28 16:00:10 +0300

    Add support for multi regions

--- ExperimentExecute/terminate_machines.py
+++ ExperimentExecute/terminate_machines.py
@@ -1,8 +1,15 @@
 import boto3
+import json
 
 with open('instances_ids', 'r+') as ids_file:
     instances_ids = ids_file.readlines()
 
-instances_ids = [x.strip() for x in instances_ids]
-client = boto3.client('ec2')
-response = client.terminate_instances(InstanceIds=instances_ids)
+
+with open('config.json') as data_file:
+        data = json.load(data_file)
+        regions = list(data['regions'].values())
+
+for idx in range(len(regions)):
+    instances_ids = [x.strip() for x in instances_ids]
+    client = boto3.client('ec2', region_name=regions[idx][:-1])
+    response = client.terminate_instances(InstanceIds=instances_ids)
