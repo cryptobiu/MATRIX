e2ea71ac0680024926c5aae7a29a7cc51aba4097
liorbiu <liork.cryptobiu@gmail.com>
2017-10-01 08:56:11 +0300

    Create report for multi number of parties works

--- ExperimentReport/analyze_results.py
+++ ExperimentReport/analyze_results.py
@@ -10,32 +10,38 @@ with open('config.json') as conf_file:
 
 results_directory = conf_data['resultsDirectory']
 files_list = glob.glob(expanduser('%s/*.json' % results_directory))
+number_of_parties = list(conf_data['numOfParties'].values())
+
+parties = set()
+for file in files_list:
+    parties.add(int(file.split('_')[4].split('.')[0].split('=')[1]))
 
 results_headers = set()
 for file in files_list:
     results_headers.add(file.split('_')[2])
 
+parties = list(parties)
 results_headers = list(results_headers)
 
-files_headers_list = list()
+parties_files_list = list()
+headers_files_list = list()
+
 
 # split files according to their task name
-for header in results_headers:
-    files_headers = list()
-    for file in files_list:
-        if header in file:
-            files_headers.append(file)
-    files_headers_list.append(files_headers)
+for party in parties:
+    for header in results_headers:
+        files_headers = list()
+        for file in files_list:
+            if header in file and 'numberOfParties=%s' % str(party) in file:
+                files_headers.append(file)
+        headers_files_list.append(files_headers)
 
-print(files_headers_list)
+print(headers_files_list)
 
 with open(files_list[0]) as data_file:
     data = json.load(data_file, object_pairs_hook=OrderedDict)
 
 
-min_num_of_parties = conf_data['minNumOfParties']
-max_num_of_parties = conf_data['maxNumOfParties']
-quanta = conf_data['partiesQuanta']
 results_directory = conf_data['resultsDirectory']
 protocol_name = conf_data['protocol']
 
@@ -45,14 +51,20 @@ style1 = wb.add_format({'num_format': '#.##'})
 ws = wb.add_worksheet(protocol_name)
 ws.write(0, 0, 'Phase/Number of Parties')
 
+counter = 0
+for party_idx in range(len(parties)):
+    ws.write(0, party_idx + 1, parties[party_idx])
+    for header_idx in range(len(results_headers)):
+        header_data = 0
+        print(header_idx + 2 * party_idx)
+        ws.write(header_idx + 1, 0, results_headers[header_idx])
+        # print(headers_files_list[header_idx])
+        for data_file in headers_files_list[header_idx + 2 * party_idx]:
+            with open(data_file, 'r') as f:
+                data = json.load(f)
+            header_data += int(data[results_headers[header_idx]]['duration'])
+        counter += 1
+        ws.write(header_idx + 1, party_idx + 1, header_data // len(headers_files_list[header_idx + 2 * party_idx]))
 
-for header_idx in range(len(results_headers)):
-    ws.write(header_idx + 1, 0, results_headers[header_idx])
-    header_data = 0
-    for data_file in files_headers_list[header_idx]:
-        with open(data_file, 'r') as f:
-            data = json.load(f)
-        header_data += int(data[results_headers[header_idx]]['duration'])
-    ws.write(header_idx + 1, 1, header_data // len(files_headers_list[header_idx]))
 
 wb.close()
