4a689401111aa35f1bb05b092c63e8346196ba3b
liorbiu <liork.cryptobiu@gmail.com>
2017-11-07 10:46:45 +0200

    Update analyze results to support the new API of libscapi Measurment

--- ExperimentReport/analyze_results.py
+++ ExperimentReport/analyze_results.py
@@ -46,68 +46,61 @@ def send_email(file_name):
 
 def analyze_results():
     results_directory = results_path
-    print('***********************')
-    print(results_path)
     files_list = glob.glob(expanduser('%s/*.json' % results_directory))
 
     parties = set()
     for file in files_list:
-        parties.add(int(file.split('_')[5].split('.')[0].split('=')[1]))
-
-    results_headers = set()
-    for file in files_list:
-        results_headers.add(file.split('_')[3])
+        parties.add(int(file.split('_')[2].split('.')[0].split('=')[1]))
 
     parties = list(parties)
-    results_headers = list(results_headers)
+    parties.sort()
 
-    headers_files_list = list()
+    # parsing tasks names from json file
+    # Assumption : all the parties measure the same tasks
 
-    # split files according to their task name
-    for party in parties:
-        for header in results_headers:
-            files_headers = list()
-            for file in files_list:
-                if header in file and 'numberOfParties=%s' % str(party) in file:
-                    files_headers.append(file)
-            headers_files_list.append(files_headers)
+    tasks_names = dict()
 
-    # results_directory = conf_data['resultsDirectory']
+    with open(files_list[0], 'r') as f:
+        data = json.load(f)
+
+    for i in range(len(data)):
+        tasks_names[data[i]['name']] = list()
+
     protocol_name = conf_data['protocol']
+    num_of_repetitions = conf_data['numOfInternalRepetitions']
 
     protocol_time = str(time.time())
     results_file_name = 'Results/Results_%s_%s.xlsx' % (protocol_name, protocol_time)
+    print(results_file_name)
     wb = xlsxwriter.Workbook(results_file_name)
     style1 = wb.add_format({'num_format': '#.##'})
 
     ws = wb.add_worksheet(protocol_name)
     ws.write(0, 0, 'Phase/Number of Parties')
 
-    counter = 0
+    # counter = 0
     for party_idx in range(len(parties)):
         ws.write(0, party_idx + 1, parties[party_idx])
-        for header_idx in range(len(results_headers)):
-            header_data = 0
-            ws.write(header_idx + 1, 0, results_headers[header_idx])
-            for data_file in headers_files_list[header_idx + len(results_headers) * party_idx]:
-                with open(data_file, 'r') as f:
-                    data = json.load(f)
-                header_data += int(data[results_headers[header_idx]]['duration'])
+
+        for data_file in files_list:
+            if 'numOfParties=%s' % str(parties[party_idx]) in data_file:
+                with open(data_file, 'r') as df:
+                    json_data = json.load(df)
+                    for json_size_idx in range(len(json_data)):
+                        for rep_idx in range(num_of_repetitions):
+                            tasks_names[json_data[json_size_idx]['name']].append(
+                                json_data[json_size_idx]['iteration_%s' % str(rep_idx)])
+        # write data to excel
+        counter = 0
+        for key in tasks_names.keys():
+            ws.write(counter + 1, 0, key, style1)
+            ws.write(counter + 1, party_idx + 1,
+                     sum(tasks_names[key]) / len(tasks_names[key]), style1)
             counter += 1
-            ws.write(header_idx + 1, party_idx + 1, header_data // len(headers_files_list[header_idx + 2 * party_idx]),
-                     style1)
 
-    # create chart
-    chart = wb.add_chart({'type': 'scatter', 'subtype': 'straight_with_markers'})
-    # categories are X axis, values are Y axis
-    # [sheetname, first_row, first_col, last_row, last_col]
-    chart.add_series({'categories': [protocol_name, 0, 0, len(results_headers), len(parties)],
-                      'values': [protocol_name, 0, 0, len(results_headers), len(parties)]}
-                     )
-    print(len(results_headers))
-    print(len(parties))
-
-    ws.insert_chart('A12', chart)
+        # delete all the data from the lists after finish iterate al over party data
+        for val in tasks_names.values():
+            val.clear()
 
     wb.close()
 
