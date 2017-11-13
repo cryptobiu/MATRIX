2f9565e09b1d6f9ae144344d97183558a0c1c790
liorbiu <liork.cryptobiu@gmail.com>
2017-11-13 16:52:34 +0200

    Remove Node js application, CLI application can be used instead

--- NodeApp/routes/reports.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-//Reports page route
-router.get('/', function (req, res) {
-    res.render('reports')
-});
-
-module.exports = router;
