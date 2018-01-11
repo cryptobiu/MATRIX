597744c61976c83a3cf8ba856314289f3d01d1cc
Lior Koskas <liork.cryptobiu@gmail.com>
2018-01-11 16:14:28 +0200

    Split SPDZ scripts to install and exection

--- ExternalProtocols/spdz_setup.sh
+++ ExternalProtocols/spdz_setup.sh
@@ -3,41 +3,21 @@
 ########################################################################################
 show_usage() {
 	echo $0" OPTIONS"
-	echo "-partyID <pid>		The party identity number"
-	echo "-n <prts>		The number of parties"
 	echo "-x			Use SPDZ extension library"
-	echo "-p			Port number"
 	echo "-h			Show usage help and exit"
 }
 
 ########################################################################################
-while getopts p:i:n:xh option
+while getopts p:i:n:s:xh option
 do
  case "${option}"
  in
  h) show_usage; exit 0;;
  x) EXT=on;;
- n) PARTIES=${OPTARG};;
- partyID) PID=$OPTARG;;
- p) PORT=$OPTARG;;
  esac
 done
 
 ########################################################################################
-re='^[0-9]+$'
-if ! [[ $PARTIES =~ $re ]]
-then
-	echo "Parties number is mandatory"; exit 1
-else
-	echo "Number of parties = "$PARTIES
-fi
-
-if ! [[ $PID =~ $re ]]
-then
-	echo "Party identity is mandatory"; exit 1
-else
-	echo "Party identity = "$PID
-fi
 
 if [ -n "$EXT" ]
 then
@@ -46,17 +26,9 @@ else
 	echo "NOT using SPDZ extension"
 fi
 
-if [ -z "$PORT" ]
-then
-	PORT=38765
-fi
-echo "Using port number "$PORT
-
 ########################################################################################
 
 cd ~
-
-########################################################################################
 git clone https://ran-proshan:GitHub1@github.com/cryptobiu/SPDZ-2.git
 
 if [ -n "$EXT" ]
@@ -78,27 +50,11 @@ then
 
 	mv ./SPDZ-2/CONFIG ./SPDZ-2/CONFIG.orig
 	mv ./SPDZ-2/CONFIG_extended ./SPDZ-2/CONFIG
-
-	BITSP=61
-else
-	BITSP=64
 fi
 
 cd ./SPDZ-2
 make clean
 make
-
-Scripts/setup-online.sh $PARTIES $BITSP 40
-
-./compile.py ubi_gfp_mult
-
-if [ $PARTIES == $PID ]
-then
-	./Server.x 2 $PORT
-else
-	INPUT=$(($PID+3))
-	echo $INPUT | ./Player-Online.x -lgp $BITSP -pn $PORT $PID ubi_gfp_mult
-fi
 
 ########################################################################################
 
