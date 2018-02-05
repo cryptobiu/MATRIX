812e73b9ffbaab3bd9ecb6b87c673c607a6f071b
Lior Koskas <liork.cryptobiu@gmail.com>
2018-02-05 08:32:12 +0200

    Add SPDZ installation

--- ExperimentExecute/pre_process.py
+++ ExperimentExecute/pre_process.py
@@ -7,7 +7,7 @@ def install_ntl():
     os.system('wget http://www.shoup.net/ntl/ntl-9.10.0.tar.gz')
     os.system('tar -xf ntl-9.10.0.tar.gz')
     os.chdir('ntl-9.10.0/src')
-    os.system('./configure')
+    os.system('./configure WIZARD=off CXXFLAGS=\"-g -O3 -fPIC\"')
     os.system('make')
 
 
@@ -17,15 +17,53 @@ def install_malicious_yao_lib():
 
 
 def install_mpir():
-    os.system('sudo apt-get install yasm m4 build-essential unzip wget -y')
+    # delete directory if exists
     os.system('rm -rf mpir-3.0.0')
-    os.system('wget http://mpir.org/mpir-3.0.0-alpha2.zip')
-    os.system('unzip mpir-3.0.0-alpha2.zip')
+    os.system('wget http://mpir.org/mpir-3.0.0.tar.bz2')
+    os.system('tar -xf mpir-3.0.0-alpha2.tar.bz2')
     os.chdir('mpir-3.0.0/')
     os.system('./configure --enable-cxx')
     os.system('make')
     os.system('make check')
     os.system('sudo make install')
+
+
+def install_spdz_stations():
+    os.system('sudo apt-get update')
+    os.system('sudo apt-get install build-essential -y')
+    os.system('sudo apt-get install cmake libgmp3-dev yasm m4 python libz-dev libsodium-dev -y')
+
+    # Install dependencies of SPDZ
+
+    install_mpir()
+    install_ntl()
+
+    # Install libscapi
+    os.chdir(expanduser('~'))
+    os.system('git clone https://github.com/cryptobiu/libscapi.git')
+    os.chdir(expanduser('~/libscapi'))
+    os.system('git checkout SPDZ')
+    os.system('make')
+
+    # Install MPCHonestMajorityForSpdz
+    os.chdir(expanduser('~'))
+    os.system('git clone https://github.com/cryptobiu/MPCHonestMajorityForSpdz.git')
+    os.chdir(expanduser('~/MPCHonestMajorityForSpdz'))
+    os.system('cmake .')
+    os.system('make')
+
+    # Install SPDZ-2_extension_library
+    os.chdir(expanduser('~'))
+    os.system('git clone https://github.com/cryptobiu/spdz-2_extension_library')
+    os.chdir(expanduser('~/spdz-2_extension_library'))
+    os.system('cmake .')
+    os.system('make')
+
+    # Install SPDZ-2
+    os.chdir(expanduser('~'))
+    os.system('git clone https://github.com/cryptobiu/SPDZ-2')
+    os.chdir(expanduser('~/SPDZ-2'))
+    os.system('make')
 
 
 task_name = sys.argv[1]
@@ -35,6 +73,6 @@ if task_name == '1':
 elif task_name == '2':
     install_malicious_yao_lib()
 elif task_name == '3':
-    install_mpir()
+    install_spdz_stations()
 else:
     raise ValueError('Invalid choice')
