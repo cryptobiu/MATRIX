dc1814776a7e5298a9e190678d904f3277bfe795
Lior Koskas <liork.cryptobiu@gmail.com>
2018-03-25 12:13:50 +0300

    PEP8

--- InstancesManagement/terminate_instances.py
+++ InstancesManagement/terminate_instances.py
@@ -22,7 +22,7 @@ class Terminate:
             client.terminate_instances(InstanceIds=instances_ids)
 
         public_ips_files = glob.glob('InstancesConfigurations/public_ips*')
-        if len(public_ips_files)>0:
+        if len(public_ips_files) > 0:
             for f in public_ips_files:
                 try:
                     os.remove(f)
@@ -30,7 +30,7 @@ class Terminate:
                     pass
 
         instances_ids_files = glob.glob('InstancesConfigurations/instances_ids*')
-        if len(instances_ids_files)>0:
+        if len(instances_ids_files) > 0:
             for f in instances_ids_files:
                 try:
                     os.remove(f)
