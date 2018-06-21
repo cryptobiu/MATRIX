20add4486699feec18f6e505746168b302b24115
liorbiu <liork.cryptobiu@gmail.com>
2018-06-21 22:24:00 +0300

    file returns as a string

--- NodeApp/routes/polls.js
+++ NodeApp/routes/polls.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pollscontrollerNew = require('./../controllers/pollsControllerNew');
+const fs = require('fs');
 
 router.get('/', function (req, res) {
     res.render('polls', { title: 'Polls' });
@@ -38,7 +39,10 @@ router.get('/parties', function (req, res) {
 router.get('/circuit/:circuitName', function (req, res) {
 
     let circuitName = req.params.circuitName;
-    res.download('public/assets/' + circuitName);
+    fs.readFile('public/assets/'+circuitName, 'utf8', function (error, data) {
+    if (error) throw error;
+    res.write(data.toString());
+});
 });
 
 router.get('/configuration', function (req, res) {
