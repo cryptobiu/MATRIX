9197d5160bade8945dcd219f09685c78727e1416
liorbiu <liork.cryptobiu@gmail.com>
2018-11-29 12:06:04 +0200

    Excel reporting also works for internal protocols

--- Reporting/analyze_results.py
+++ Reporting/analyze_results.py
@@ -78,11 +78,76 @@ class Analyze:
         server.sendmail(me, users, message.as_string())
         server.quit()
 
-    def analyze_results(self):
+    def analyze_json(self, files_list):
         protocol_name = self.protocol_config['protocol']
         results_path = self.protocol_config['resultsDirectory']
 
-        files_list = glob.glob(expanduser('%s/*.log' % results_path))
+        parties = set()
+        parties_files = OrderedDict()
+
+        for file in files_list:
+            number_of_parties = basename(file.split('*')[3])
+            parties.add(int(number_of_parties))
+            if number_of_parties not in parties_files.keys():
+                parties_files[number_of_parties] = []
+            parties_files[number_of_parties].append(file)
+
+        parties = list(parties)  # sort parties in ascending order
+        parties.sort()
+
+        # map between number of parties and files names
+
+        # parsing tasks names from json file
+        # Assumption : all the parties measure the same tasks
+
+        tasks_names = dict()
+
+        # load one of the data files to receive the headers to the xlsx file
+
+        with open(files_list[0], 'r') as f:
+            data = json.load(f)
+
+        # init list values
+        for i in range(len(data)):
+            tasks_names[data[i]['name']] = list()
+
+        num_of_repetitions = self.protocol_config['numOfInternalRepetitions']
+        results_file_name = expanduser('%s/Results_%s.xlsx' % (results_path, protocol_name))
+
+        wb = Workbook(write_only=False)  # append to file
+        ws = wb.create_sheet('Results')
+        wb.remove(wb['Sheet'])  # Remove the default sheet
+        ws.cell(row=1, column=1, value='Phase/Number of Parties')
+
+        files_list.sort()
+
+        # counter = 0
+        for party_idx in range(len(parties)):
+            ws.cell(row=1, column=party_idx + 2, value=parties[party_idx])
+            party_size = parties[party_idx]
+            for data_file in parties_files[str(party_size)]:
+                with open(data_file, 'r') as df:
+                    json_data = json.load(df, object_pairs_hook=OrderedDict)
+                    for json_size_idx in range(len(json_data)):
+                        for rep_idx in range(num_of_repetitions):
+                            tasks_names[json_data[json_size_idx]['name']].append(
+                                json_data[json_size_idx]['iteration_%s' % str(rep_idx)])
+            # write data to excel
+            counter = 1
+            for key in tasks_names.keys():
+                ws.cell(row=counter + 1, column=1, value=key)
+                ws.cell(row=counter + 1, column=party_idx + 2, value=sum(tasks_names[key]) / len(tasks_names[key]))
+                counter += 1
+
+            # delete all the data from the lists after finish iterate al over party data
+            for val in tasks_names.values():
+                val.clear()
+
+        wb.save(results_file_name)
+
+    def analyze_logs(self, files_list):
+        protocol_name = self.protocol_config['protocol']
+        results_path = self.protocol_config['resultsDirectory']
         parties = set()
         parties_files = OrderedDict()
 
@@ -104,7 +169,7 @@ class Analyze:
         parties = list(parties)
         parties.sort()  # sort parties in ascending order
         # define result file
-        result_file_name = expanduser('%s/Results_%s.xlsx' % (results_path, protocol_name))
+        results_file_name = expanduser('%s/Results_%s.xlsx' % (results_path, protocol_name))
 
         wb = Workbook(write_only=False)  # append to file
 
@@ -119,10 +184,10 @@ class Analyze:
             tasks_counter += 1
 
         for party_idx in range(len(parties)):
-            party_id = parties[party_idx]
-            ws.cell(row=1, column=party_idx + 2, value=party_id)
-            for idx in range(len(parties_files[str(party_id)])):
-                with open(parties_files[str(party_id)][idx]) as data_file:
+            party_size = parties[party_idx]
+            ws.cell(row=1, column=party_idx + 2, value=party_size)
+            for idx in range(len(parties_files[str(party_size)])):
+                with open(parties_files[str(party_size)][idx]) as data_file:
                     # get only the values for each task
                     data = [l.rstrip('\n').split(':')[1].split(',')[:-1] for l in data_file.readlines()[1:]]
                     counter = 0
@@ -142,7 +207,19 @@ class Analyze:
                 val.clear()
 
         # save the excel file
-        wb.save(result_file_name)
+        wb.save(results_file_name)
+
+    def analyze_results(self):
+        results_path = self.protocol_config['resultsDirectory']
+
+        external_protocol = json.loads(self.protocol_config['isExternal'].lower())
+        if external_protocol:
+            files_list = glob.glob(expanduser('%s/*.log' % results_path))
+            self.analyze_logs(files_list)
+        else:
+            files_list = glob.glob(expanduser('%s/*cpu*.json' % results_path))
+            self.analyze_json(files_list)
+
 
 
 
