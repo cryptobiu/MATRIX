169d207c986f38885365cb192adce823b438ccb0
liorbiu <liork.cryptobiu@gmail.com>
2018-12-30 16:54:07 +0200

    Add missing protocol class-needed for form submission

---
+++ FlaskApp/frontend/src/app/protocol.ts
@@ -0,0 +1,8 @@
+export class Protocol {
+  constructor(
+    public protocolName: string,
+    public repoAddress: string,
+    public securityLevel: string,
+    public thresholdLevel: string,
+  ){}
+}
