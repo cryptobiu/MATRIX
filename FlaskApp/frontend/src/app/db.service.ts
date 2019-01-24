3f597a14293e83c7e56d0fc521ce3d863b7ecfca
liorbiu <liork.cryptobiu@gmail.com>
2019-01-24 11:40:46 +0200

    Add Elasticsearch service

--- FlaskApp/frontend/src/app/db.service.ts
+++ FlaskApp/frontend/src/app/db.service.ts
@@ -2,9 +2,8 @@ import {Injectable, NgModule} from '@angular/core';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import {AppComponent} from "./app.component";
-import {Observable} from "rxjs";
+import {Observable, Observer, Subject} from "rxjs";
 import {ICompetition, IProtocol, IProtocolData} from "./interfaces";
-
 
 @NgModule({
   imports: [CommonModule, HttpClientModule],
@@ -37,8 +36,8 @@ export class DbService {
     return this._http.get<IProtocolData>(url);
   }
 
-  executeProtocol(protocolName: string) {
-    let url = this._urlApi + 'protocols/' + protocolName;
+  executeDeployOperation(protocolName: string, operation:string) {
+    let url = this._urlApi + 'deploy/' + protocolName + '/' + operation;
     return this._http.get(url);
   }
 
