67099aa7ce93320c2393891fd993e21767af3344
liorbiu <liork.cryptobiu@gmail.com>
2019-02-27 12:42:10 +0200

    Add error handling

--- FlaskApp/frontend/src/app/Services/db.service.ts
+++ FlaskApp/frontend/src/app/Services/db.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from "rxjs";
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable, throwError} from "rxjs";
 import {ICompetition, IProtocol} from "../interfaces";
+import {catchError} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -25,7 +26,7 @@ export class DbService {
 
   getProtocols(): Observable<IProtocol[]> {
     let url = this._urlApi + 'protocols';
-    return this._http.get<IProtocol[]>(url);
+    return this._http.get<IProtocol[]>(url).pipe(catchError(this.handleError));
   }
 
   executeDeployOperation(protocolName: string, operation:string) {
@@ -42,5 +43,21 @@ export class DbService {
     let url = this._urlApi + 'reporting/' + protocolName + '/' + operation;
     return this._http.get(url);
   }
+
+  private handleError(error: HttpErrorResponse) {
+  if (error.error instanceof ErrorEvent) {
+    // A client-side or network error occurred. Handle it accordingly.
+    console.error('An error occurred:', error.error.message);
+  } else {
+    // The backend returned an unsuccessful response code.
+    // The response body may contain clues as to what went wrong,
+    console.error(
+      `Backend returned code ${error.status}, ` +
+      `body was: ${error.error}`);
+  }
+  // return an observable with a user-facing error message
+  return throwError(
+    'Something bad happened; please try again later.');
+};
 
 }
