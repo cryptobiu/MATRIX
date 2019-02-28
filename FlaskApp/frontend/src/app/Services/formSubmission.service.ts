0d849db5d8bc3f21fb101cab1347bad3763a9d69
liorbiu <liork.cryptobiu@gmail.com>
2019-02-28 11:45:35 +0200

    Add error handling for all API requests..

--- FlaskApp/frontend/src/app/Services/formSubmission.service.ts
+++ FlaskApp/frontend/src/app/Services/formSubmission.service.ts
@@ -33,7 +33,9 @@ export class FormSubmissionService {
     // The response body may contain clues as to what went wrong,
     console.error(
       `Backend returned code ${error.status}, ` +
-      `body was: ${error.error}`);
+      `body error was: ${error.error}, ` +
+      `body was:  ${error.message}`
+      );
   }
   // return an observable with a user-facing error message
   return throwError(
