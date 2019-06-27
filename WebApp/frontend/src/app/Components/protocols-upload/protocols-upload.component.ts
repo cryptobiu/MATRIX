d062fa19c1c2040390e3c2dac39394f5569d8b48
liorbiu <liork.cryptobiu@gmail.com>
2019-06-27 22:30:08 +0300

    Add all the regions

--- WebApp/frontend/src/app/Components/protocols-upload/protocols-upload.component.ts
+++ WebApp/frontend/src/app/Components/protocols-upload/protocols-upload.component.ts
@@ -27,9 +27,17 @@ export class ProtocolsUploadComponent implements OnInit {
   securityThreshold = ['None', 'Honest Majority', '2/3 Majority'];
   cloudProvidersName = ['AWS', 'Azure'];
   selectedCP = '';
-  awsRegions = ['us-east-1', 'ue-west-2'];
+  awsRegions = ['us-east-2', 'us-east-1', 'us-west-1', 'us-west-2', 'ap-east-1', 'ap-south-1', 'ap-northeast-3',
+    'ap-northeast-2', 'ap-southeast-1', 'ap-southeast-1', 'ap-northeast-1', 'ca-central-1', 'eu-central-1',
+    'eu-west-1', 'eu-west-2', 'ue-west-3', 'eu-north-1', 'sa-east-1 '];
   awsInstances = ['c5.large', 'c5.xlarge', 'c2.2xlarge', 'c5.4xlarge', 'c5.9xlarge', 'c5.18xlarge'];
-  azureRegions = ['East US', 'Brazil South'];
+
+  azureRegions = ['Central US', 'East US 2', 'East US', 'North Central US', 'South Central US', 'West US 2',
+    'West Central US', 'West US', 'Canada Central', 'Canada East', 'Brazil South', 'North Europe', 'West Europe',
+    'France Central', 'France South', 'UK South', 'UK West', 'Germany Central', 'Germany Northeast', 'East Asia',
+    'Southeast Asia', 'Australia Central', 'Australia Central 2', 'Australia East', 'Australia Southeast', 'China East',
+    'China North', 'China East 2', 'China North 2', 'Central India', 'South India', 'West India', 'Japan East',
+    'Japan West', 'Korea Central', 'Korea South', 'South Africa North', 'South Africa West', 'UAE Central', 'UAE North'];
   azureInstances = ['Standard_F1s', 'Standard_F2s', 'Standard_F4s', 'Standard_F8s', 'Standard_F16s'];
   regions = [];
   numOfParties = 2;
